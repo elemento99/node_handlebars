3c658a85a03e874804c06cc6a284757b26a097e9
hajime2 <joaquin.gonzalezparada@gmail.com>
2024-04-12 13:54:21 -0400

    tercer commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import { engine } from 'express-handlebars';
 
+import { services } from './data/services.data.js'
+
+
 const app = express();
 
 
@@ -19,12 +22,27 @@ app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { title: "Home page 2.0" });
 });
 
 
 app.get('/services', (req, res) => {
-    res.render('services');
+res.render('services', {services: services})
+})
+ 
+app.get('/services/:name', (req, res) => {
+    
+    
+    const nameURL = req.params.name
+
+    //cómo buscar algo dentro de un array? re: find
+
+    const service = services.find((item) => item.url === `/services/${nameURL}` )
+    console.log(service)
+
+        res.render('service', {service})
+
+    
 });
 
 
